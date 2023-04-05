7ef2529316956419b6961df625f109e197484401
Author <inburek@users.noreply.github.com>
2023-04-05 13:41:18 +0100

    Added failing tests for more complex carts.

--- src/test/scala/inburek/interviews/mercator/i20230405/ShoppingCartTest.scala
+++ src/test/scala/inburek/interviews/mercator/i20230405/ShoppingCartTest.scala
@@ -9,6 +9,21 @@ final class ShoppingCartTest extends AnyFreeSpecLike {
       "for empty cart" in {
         ShoppingCart(List.empty).total shouldBe Pence(0)
       }
+      "for cart with one apple" in {
+        ShoppingCart(List(Fruit.Apple)).total shouldBe Pence(60)
+      }
+      "for cart with one orange" in {
+        ShoppingCart(List(Fruit.Orange)).total shouldBe Pence(25)
+      }
+      "for cart with two apples" in {
+        ShoppingCart(List(Fruit.Apple, Fruit.Apple)).total shouldBe Pence(120)
+      }
+      "for cart with two oranges" in {
+        ShoppingCart(List(Fruit.Orange, Fruit.Orange)).total shouldBe Pence(50)
+      }
+      "for cart with two apples and three oranges" in {
+        ShoppingCart(List(Fruit.Apple, Fruit.Apple, Fruit.Orange, Fruit.Orange, Fruit.Orange)).total shouldBe Pence(120 + 75)
+      }
     }
   }
 }
