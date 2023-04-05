05b398a5c5aa9ad20bf1286a96c000eded7113be
Author <inburek@users.noreply.github.com>
2023-04-05 14:20:09 +0100

    Implement simple offers.

--- src/main/scala/inburek/interviews/mercator/i20230405/ShoppingCart.scala
+++ src/main/scala/inburek/interviews/mercator/i20230405/ShoppingCart.scala
@@ -4,7 +4,16 @@ import cats.data.ValidatedNel
 import cats.implicits._
 
 final case class ShoppingCart(items: List[Fruit]) {
-  def total: Pence = items.foldLeft(Pence(0))((acc, fruit) => acc + Pricing.price(fruit))
+  def total: Pence = {
+    val counts: Map[Fruit, Int] = items.groupBy(identity).view.mapValues(_.size).toMap
+
+    val prices: Map[Fruit, Pence] = counts.map {
+      case (fruit @ Fruit.Apple, count) => (fruit, Pricing.price(fruit) * (count / 2 + count % 2))
+      case (fruit @ Fruit.Orange, count) => (fruit, Pricing.price(fruit) * (count / 3 * 2 + count % 3))
+    }
+
+    prices.values.foldLeft(Pence(0))(_ + _)
+  }
 }
 object ShoppingCart {
   def fromStrings(items: List[String]): ValidatedNel[String, ShoppingCart] = {
