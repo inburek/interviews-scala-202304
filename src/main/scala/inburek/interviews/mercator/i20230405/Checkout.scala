9ef3584551725c5950bc86bede1798c6d5172fd8
Author <inburek@users.noreply.github.com>
2023-04-05 13:42:01 +0100

    Cart price for all examples.

--- src/main/scala/inburek/interviews/mercator/i20230405/Checkout.scala
+++ src/main/scala/inburek/interviews/mercator/i20230405/Checkout.scala
@@ -1,5 +1,5 @@
 package inburek.interviews.mercator.i20230405
 
 final case class ShoppingCart(items: List[Fruit]) {
-  def total: Pence = Pence(0)
+  def total: Pence = items.foldLeft(Pence(0))((acc, fruit) => acc + Pricing.price(fruit))
 }
