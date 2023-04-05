3ec2e2433305e1c469ffffe1cfe4ad9e41d4aadd
Author <inburek@users.noreply.github.com>
2023-04-05 13:52:09 +0100

    Add incomplete implementation to make shopping carts from strings.

--- src/main/scala/inburek/interviews/mercator/i20230405/Checkout.scala
+++ src/main/scala/inburek/interviews/mercator/i20230405/Checkout.scala
@@ -1,5 +1,12 @@
 package inburek.interviews.mercator.i20230405
 
+import cats.data.{Validated, ValidatedNel}
+
 final case class ShoppingCart(items: List[Fruit]) {
   def total: Pence = items.foldLeft(Pence(0))((acc, fruit) => acc + Pricing.price(fruit))
+}
+object ShoppingCart {
+  def fromStrings(items: List[String]): ValidatedNel[String, ShoppingCart] = {
+    Validated.validNel(ShoppingCart(Nil))
+  }
 }
--- src/test/scala/inburek/interviews/mercator/i20230405/ShoppingCartTest.scala
+++ src/test/scala/inburek/interviews/mercator/i20230405/ShoppingCartTest.scala
@@ -1,10 +1,16 @@
 package inburek.interviews.mercator.i20230405
 
+import cats.data.Validated
 import org.scalatest.freespec.AnyFreeSpecLike
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 final class ShoppingCartTest extends AnyFreeSpecLike {
   "ShoppingCart" - {
+    ".fromStrings" - {
+      "for empty list" in {
+        ShoppingCart.fromStrings(Nil) shouldBe Validated.validNel(ShoppingCart(Nil))
+      }
+    }
     ".total" - {
       "for empty cart" in {
         ShoppingCart(List.empty).total shouldBe Pence(0)
