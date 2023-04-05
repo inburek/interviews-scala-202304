db06ccef3d41cf9d433e3bc1dbc19cf0f97c230b
Author <inburek@users.noreply.github.com>
2023-04-05 13:32:03 +0100

    Add pricing information for apples and oranges.

--- src/main/scala/inburek/interviews/mercator/i20230405/Domain.scala
+++ src/main/scala/inburek/interviews/mercator/i20230405/Domain.scala
@@ -3,7 +3,12 @@ package inburek.interviews.mercator.i20230405
 final case class Pence(value: Int)
 
 case object Pricing {
-  def price(fruit: Fruit): Pence = ???
+  def price(fruit: Fruit): Pence = {
+    fruit match {
+      case Fruit.Apple => Pence(60)
+      case Fruit.Orange => Pence(25)
+    }
+  }
 }
 
 sealed abstract class Fruit private(val name: String)
