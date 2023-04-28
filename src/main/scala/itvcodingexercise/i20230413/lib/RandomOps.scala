76d798b0f828cbd38fbcecd13b4a7cbe758f7a1a
Author <inburek@users.noreply.github.com>
2023-04-28 11:37:35 +0100

    More discussion with ITV about unit testing.

--- src/main/scala/itvcodingexercise/i20230413/lib/RandomOps.scala
+++ src/main/scala/itvcodingexercise/i20230413/lib/RandomOps.scala
@@ -1,3 +1,9 @@
-package itvcodingexercise.i20230413.lib object RandomOps {
+package itvcodingexercise.i20230413.lib
 
+import scala.util.Random
+
+object RandomOps {
+  implicit final class RichRandom(random: Random) {
+    def chooseElement[T](seq: Seq[T]): Option[T] = ???
+  }
 }
